9fdc1929d142aa6b14a537467fad4d51c3fa04dc
abiudy <nyagaka254@gmail.com>
2024-08-26 20:25:01 +0300

    added lsp and formatter for bash in nvim

--- nvim/.config/nvim/lua/abiudy/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/abiudy/plugins/none-ls.lua
@@ -2,6 +2,7 @@ return {
   "nvimtools/none-ls.nvim",
   dependencies = {
     "nvimtools/none-ls-extras.nvim",
+    "gbprod/none-ls-shellcheck.nvim",
   },
   config = function()
     local null_ls = require("null-ls")
@@ -11,7 +12,8 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.formatting.shfmt,
-        -- require("none-ls.diagnostics.eslint"),
+        require("none-ls-shellcheck.code_actions"),
+        -- require("none-ls-shellcheck.diagnostics"),
       },
     })
 
