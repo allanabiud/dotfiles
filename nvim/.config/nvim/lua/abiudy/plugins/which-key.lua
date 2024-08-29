1102aa754cea2ccd705a99a4072990eb810ae0b5
abiudy <nyagaka254@gmail.com>
2024-08-29 18:18:07 +0300

    nvim which-key updates

--- nvim/.config/nvim/lua/abiudy/plugins/which-key.lua
+++ nvim/.config/nvim/lua/abiudy/plugins/which-key.lua
@@ -3,34 +3,110 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy", -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require("which-key").setup({
-        { "<leader>L", group = "[L]azygit" },
-        { "<leader>L_", hidden = true },
-        { "<leader>R", group = "[R]un" },
-        { "<leader>R_", hidden = true },
-        { "<leader>T", group = "[T]odo" },
-        { "<leader>T_", hidden = true },
-        { "<leader>c", group = "[c]ode" },
-        { "<leader>c_", hidden = true },
-        { "<leader>d", group = "[d]iagnostics" },
-        { "<leader>d_", hidden = true },
-        { "<leader>f", group = "[f]ormat" },
-        { "<leader>f_", hidden = true },
-        { "<leader>h", group = "Git [h]unk" },
-        { "<leader>h_", hidden = true },
-        { "<leader>l", group = "[l]int" },
-        { "<leader>l_", hidden = true },
-        { "<leader>r", group = "[r]ename" },
-        { "<leader>r_", hidden = true },
-        { "<leader>s", group = "[s]earch" },
-        { "<leader>s_", hidden = true },
-        { "<leader>t", group = "[t]oggle" },
-        { "<leader>t_", hidden = true },
-        { "<leader>w", group = "[w]orkspace" },
-        { "<leader>w_", hidden = true },
+      require("which-key").add({
+        {
+          "<leader>L",
+          group = "[L]azygit",
+        },
+        {
+          "<leader>L_",
+          hidden = true,
+        },
+        {
+          "<leader>R",
+          group = "[R]un",
+        },
+        {
+          "<leader>R_",
+          hidden = true,
+        },
+        {
+          "leader>T",
+          group = "[T]odo",
+        },
+        {
+          "<leader>T_",
+          hidden = true,
+        },
+        {
+          "<leader>c",
+          group = "[c]ode",
+        },
+        {
+          "<leader>c_",
+          hidden = true,
+        },
+        {
+          "<leader>d",
+          group = "[d]iagnostics",
+        },
+        {
+          "<leader>d_",
+          hidden = true,
+        },
+        {
+          "<leader>f",
+          group = "[f]ormat",
+        },
+        {
+          "<leader>f_",
+          hidden = true,
+        },
+        {
+          "<leader>h",
+          group = "Git [h]unk",
+        },
+        {
+          "<leader>h_",
+          hidden = true,
+        },
+        {
+          "<leader>l",
+          group = "[l]int",
+        },
+        {
+          "<leader>l_",
+          hidden = true,
+        },
+        {
+          "<leader>r",
+          group = "[r]ename",
+        },
+        {
+          "<leader>r_",
+          hidden = true,
+        },
+        {
+          "<leader>s",
+          group = "[s]earch",
+        },
+        {
+          "<leader>s_",
+          hidden = true,
+        },
+        {
+          "<leader>t",
+          group = "[t]oggle",
+        },
+        {
+          "<leader>t_",
+          hidden = true,
+        },
+        {
+          "<leader>w",
+          group = "[w]orkspace",
+        },
+        {
+          "<leader>w_",
+          hidden = true,
+        },
         -- visual mode
         {
-          { "<leader>h", desc = "Git [H]unk", mode = "v" },
+          {
+            "<leader>h",
+            desc = "Git [H]unk",
+            mode = "v",
+          },
         },
       })
     end,
