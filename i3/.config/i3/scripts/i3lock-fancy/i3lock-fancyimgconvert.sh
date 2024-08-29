685c0a9399fc9020349087dbd57834e59433a4c6
abiudy <nyagaka254@gmail.com>
2024-08-29 12:02:36 +0300

    deleted scripts folder

--- i3/.config/i3/scripts/i3lock-fancy/i3lock-fancyimgconvert.sh
+++
@@ -1,6 +0,0 @@
-#!bin/sh
-# Add a lock icon and text to the centre of an image
-convert /home/abiudy/Pictures/Wallpapers/spiderman-into-the-spider-verse-2018-movie-ey-1366x768.jpg -font Liberation-Sans \
-  -pointsize 26 -fill white -gravity center \
-  -annotate +0+160 "Type Password to Unlock" './icons/lock.png' \
-  -gravity center -composite newimage.png
