7e234009b3347d8c79dc0799b037b37dec14c7b6
abiudy <nyagaka254@gmail.com>
2024-09-24 09:45:20 +0300

    started using tmux and added vim-tmux-navigator

---
+++ nvim/.config/nvim/lua/abiudy/plugins/nvim-tmux-navigator.lua
@@ -0,0 +1,8 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  vim.keymap.set("n", "<c-left>", ":TmuxNavigateLeft<cr>"),
+  vim.keymap.set("n", "<c-right>", ":TmuxNavigateRight<cr>"),
+  vim.keymap.set("n", "<c-down>", ":TmuxNavigateDown<cr>"),
+  vim.keymap.set("n", "<c-up>", ":TmuxNavigateUp<cr>"),
+  vim.keymap.set("n", "<c-\\>", ":TmuxNavigatePrevious<cr>"),
+}
