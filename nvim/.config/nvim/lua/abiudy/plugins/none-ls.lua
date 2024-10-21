5729d92448f479c95882e6c5fb042a4fcb638090
abiudy <nyagaka254@gmail.com>
2024-10-21 18:46:24 +0300

    Updates
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates
    
    Updates

--- nvim/.config/nvim/lua/abiudy/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/abiudy/plugins/none-ls.lua
@@ -15,7 +15,6 @@ return {
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.isort,
         null_ls.builtins.diagnostics.mypy,
-        null_ls.builtins.diagnostics.ruff,
         require("none-ls-shellcheck.code_actions"),
         require("none-ls-shellcheck.diagnostics"),
       },
