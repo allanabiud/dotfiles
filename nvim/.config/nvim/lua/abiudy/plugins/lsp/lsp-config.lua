d692a972822bfb3a143f09aacf49b46f5d9ca111
abiudy <nyagaka254@gmail.com>
2024-10-21 21:00:57 +0300

    improved configuration of installed lsps

--- nvim/.config/nvim/lua/abiudy/plugins/lsp/lsp-config.lua
+++ nvim/.config/nvim/lua/abiudy/plugins/lsp/lsp-config.lua
@@ -8,6 +8,7 @@ return {
   },
   config = function()
     local lspconfig = require("lspconfig")
+    local mason_lspconfig = require("mason-lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local keymap = vim.keymap -- for conciseness
 
@@ -71,37 +72,31 @@ return {
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    lspconfig.lua_ls.setup({
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          -- make the language server recognize "vim" global
-          diagnostics = {
-            globals = { "vim" },
+    mason_lspconfig.setup_handlers({
+      -- default handler for installed servers
+      function(server_name)
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
+
+      ["lua_ls"] = function()
+        -- configure lua server (with special settings)
+        lspconfig["lua_ls"].setup({
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              -- make the language server recognize "vim" global
+              diagnostics = {
+                globals = { "vim" },
+              },
+              completion = {
+                callSnippet = "Replace",
+              },
+            },
           },
-          completion = {
-            callSnippet = "Replace",
-          },
-        },
-      },
-    })
-    lspconfig.html.setup({
-      capabilities = capabilities,
-      filetypes = { "html" },
-    })
-    lspconfig.cssls.setup({
-      capabilities = capabilities,
-      filetypes = { "css", "html" },
-    })
-    lspconfig.marksman.setup({
-      capabilities = capabilities,
-    })
-    lspconfig.bashls.setup({
-      capabilities = capabilities,
-    })
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-      filetypes = { "python" },
+        })
+      end,
     })
   end,
 }
