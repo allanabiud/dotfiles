ac26224e67931ec25598d31e0e4a80056404115b
abiudy <nyagaka254@gmail.com>
2025-01-24 00:55:02 +0300

    waybar tweaks
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks
    
    waybar tweaks

--- nvim/.config/nvim/lua/abiudy/plugins/productivity/auto-session.lua
+++ nvim/.config/nvim/lua/abiudy/plugins/productivity/auto-session.lua
@@ -4,16 +4,17 @@ return {
     local auto_session = require("auto-session")
 
     auto_session.setup({
+      auto_restore = false,
       log_level = "error",
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
       session_lens = {
-        -- If load_on_setup is set to false, one needs to eventually call `require("auto-session").setup_session_lens()` if they want to use session-lens.
+        buftypes_to_ignore = {},
         load_on_setup = true,
-        theme_conf = { border = true },
         previewer = false,
-        buftypes_to_ignore = {}, -- list of buffer types that should not be deleted from current session when a new one is loaded
+        theme_conf = {
+          border = true,
+        },
       },
+      suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
     })
 
     vim.keymap.set("n", "<leader>wl", require("auto-session.session-lens").search_session, { desc = "List Sessions" }) -- list all saved sessions
