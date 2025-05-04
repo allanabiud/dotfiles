2210a206b9989e267513b3fb52820e29dd110a21
allanabiud <nyagaka254@gmail.com>
2025-05-04 17:03:40 +0300

    updated nvim keymaps

--- nvim/.config/nvim/lua/abiudy/core/keymaps.lua
+++ nvim/.config/nvim/lua/abiudy/core/keymaps.lua
@@ -1,7 +1,11 @@
+local opts = { silent = true, noremap = true }
+
+-- Leader key
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- clear search highlights
-vim.keymap.set("n", "nh", ":nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<C-c", ":nohl<CR>", { desc = "Clear search highlights", silent = true })
 
 -- Move window focus
 vim.keymap.set("n", "<C-left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
@@ -12,3 +16,11 @@ vim.keymap.set("n", "<C-up>", "<C-w><C-k>", { desc = "Move focus to the upper wi
 -- Comment
 vim.keymap.set("n", "<C-/>", "gcc", { desc = "toggle comment", remap = true })
 vim.keymap.set("v", "<C-/>", "gc", { desc = "toggle comment", remap = true })
+
+-- Tabbing
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Move line up/down
+vim.keymap.set("v", "<S-up>", ":m '<-2<CR>gv=gv", { desc = "Move line down in visual mode" })
+vim.keymap.set("v", "<S-down>", ":m '>+1<CR>gv=gv", { desc = "Move line up in visual mode" })
