c01c6096974e16e18b16b15b87796a63096018d4
allanabiud <nyagaka254@gmail.com>
2025-10-16 16:55:07 +0300

    niri
    niri

--- stow/mangowc/.config/mango/autostart.sh
+++ stow/mangowc/.config/mango/autostart.sh
@@ -23,3 +23,16 @@ mpDris2 &
 
 # pywal
 wal -R &
+
+# pcmanfm daemon
+pcmanfm --daemon-mode &
+
+# Clipboard Manager
+wl-paste --type text --watch cliphist store
+wl-paste --type image --watch cliphist store
+
+# Polkit agent
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+# xdg-desktop-portal-wlr
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
