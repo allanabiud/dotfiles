9b63dd697201ed93d43e53dbb66209d5313fa41e
allanabiud <nyagaka254@gmail.com>
2026-03-11 12:46:44 +0300

    rmpc notify
    rmpc notify

--- scripts/rmpc/notify.sh
+++ scripts/rmpc/notify.sh
@@ -7,11 +7,14 @@ TMP_DIR="/tmp/rmpc"
 mkdir -p "$TMP_DIR"
 
 # Generate a unique filename for each notification
-ALBUM_ART_PATH="$TMP_DIR/notification_cover"
+ALBUM_ART_PATH="$TMP_DIR/cover_$(date +%s%N).jpg"
 
 # Path to fallback album art if no album art is found by rmpc/mpd
 # Change this to your needs
 DEFAULT_ALBUM_ART_PATH="$TMP_DIR/default_album_art.jpg"
+
+# Cleanup old covers to prevent /tmp from filling up
+rm -f $TMP_DIR/cover_*.jpg
 
 # Save album art of the currently playing song to a file
 if ! rmpc albumart --output "$ALBUM_ART_PATH"; then
@@ -19,8 +22,6 @@ if ! rmpc albumart --output "$ALBUM_ART_PATH"; then
   ALBUM_ART_PATH="${DEFAULT_ALBUM_ART_PATH}"
 fi
 
-# Send the notification using dunstify with a tag
+# Send the notification
 notify-send -i "${ALBUM_ART_PATH}" \
-  -h string:x-dunst-stack-tag:now-playing \
-  -h string:synchronous:now-playing \
   "Now Playing" "$ARTIST - $TITLE"
